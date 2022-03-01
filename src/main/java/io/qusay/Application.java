d43fd9cfc82c209e531d6e61d66d9bd4352b1bf8
qusay <qskhalifa@gmail.com>
2022-03-01 04:04:07 -0800

    Initial commit
    Signed-off-by: qusay <qskhalifa@gmail.com>

--- src/main/java/io/qusay/Application.java
+++ src/main/java/io/qusay/Application.java
@@ -2,8 +2,10 @@ package io.qusay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
